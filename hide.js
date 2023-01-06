82ce7993e80aea332124e4a8bf02e75dc2b97449
Umesh <mathulkar98@gmail.com>
2023-01-06 00:43:59 -0800

    updated03

--- hide.js
+++ hide.js
@@ -1,15 +1,22 @@
 let names = document.getElementById("names")
 
-function Hide(){
+function Hide() {
     document.getElementById("name").classList.add("hide");
     document.getElementById("unhide").classList.remove("opac");
     // let names = document.getElementById("names")
-    document.getElementById("rname").innerHTML= `<h5>Hello ${names.value}</h5><h1>i'm <br>Umesh Mathulkar</h1>`    
+    document.getElementById("rname").innerHTML = `<h5>Hello ${names.value}</h5><h1>i'm <br>Umesh Mathulkar</h1>`
 };
 
-names.addEventListener("keypress",function(event){
-    if(event.key==="Enter"){
+names.addEventListener("keypress", function (event) {
+    if (event.key === "Enter") {
         event.preventDefault();
         document.getElementById("but").click();
     }
 });
+
+function link() {
+    document.getElementById("project").classList.remove("hide");
+};
+function outlink() {
+    document.getElementById("project").classList.add("hide");
+};
